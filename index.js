cd647d555390c2bfaac60637f3a7dcd368573b24
Joshua Goldberg <josh@fullscreenmario.com>
2018-07-15 14:33:24 -0700

    Improved inline code span handling
    Fixes #2. Fixes #3.

--- index.js
+++ index.js
@@ -1,38 +1,83 @@
 const shared = require("markdownlint/lib/shared");
 
-const isAlphabetCharacter = (char) => {
+const isCapitalizedAlphabetCharacter = (char) => {
   const charCode = char.charCodeAt(0);
 
   return charCode >= "A".charCodeAt(0) && charCode <= "Z".charCodeAt(0);
+};
+
+const getNextIndexNotInCode = (line, i) => {
+  if (line[i] !== "`") {
+    return i;
+  }
+
+  i += 1;
+
+  // Get to the inside of this inline code segment
+  while (line[i] === "`") {
+    i += 1;
+
+    if (i === line.length) {
+      return undefined;
+    }
+  }
+
+  // Get to the end of the inline code segment
+  while (true) {
+    i = line.indexOf("`", i);
+
+    if (i === -1) {
+      return undefined;
+    }
+
+    if (line[i - 1] !== "\\") {
+      break;
+    }
+  }
+
+  while (line[i] === "`") {
+    i += 1;
+
+    if (i === line.length) {
+      return undefined;
+    }
+  }
+
+  return i;
+}
+
+const visitLine = (line, lineIndex, onError) => {
+  for (let i = 0; i < line.length - 2; i += 1) {
+    i = getNextIndexNotInCode(line, i);
+    if (i === undefined) {
+      return;
+    }
+
+    if (line[i] === "." && line[i + 1] === " " && isCapitalizedAlphabetCharacter(line[i + 2])) {
+      shared.addError(onError, lineIndex, null, line.substr(Math.max(0, i - 3), 7));
+    }
+  }
 };
 
 module.exports = {
   "names": [ "sentences-per-line" ],
   "description": "Each sentence should be on its own line",
   "tags": [ "sentences" ],
-  "function": function MD046(params, onError) {
+  "function": (params, onError) => {
     shared.makeTokenCache(params);
-    let inFence = false;
+    let inFenceLine = false;
 
-    shared.forEachLine(function forLine(line, lineIndex) {
+    shared.forEachLine((line, lineIndex) => {
       if (line.substring(0, 3) === "```") {
-        inFence = !inFence;
+        inFenceLine = !inFenceLine;
         return;
       }
 
-      if (inFence) {
+      if (inFenceLine) {
         return;
       }
 
-      for (let i = 0; i < line.length - 2; i += 1) {
-        if (line[i] === "`" && (i === 0 || line[i - 1] === "\\")) {
-          i = line.indexOf("`", i);
-        }
-
-        if (line[i] === "." && line[i + 1] === " " && isAlphabetCharacter(line[i + 2])) {
-          shared.addError(onError, lineIndex, null, line.substr(i, 2));
-        }
-      }
+      visitLine(line, lineIndex, onError);
     });
   }
 };
